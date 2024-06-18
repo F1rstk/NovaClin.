0713d6d47a3f8db00404f5f59fde182e12eff1f3
Filipe <filipe.sdiniz@senacsp.edu.br>
2024-06-18 17:17:44 -0300

    Concluido a criação das tabelas

--- Consulta #2.sql
+++ Consulta #2.sql
@@ -28,37 +28,22 @@ cep CHAR(8) NULL,
 observações VARCHAR(100)
 );
 
+CREATE TABLE especialidade(
+idEspecialidade INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+nomeEspecialidade VARCHAR(30) NOT NULL
+);
+
+
 CREATE TABLE medico(
 idMedico INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 IdEspecialidade INT NOT NULL,
 nome VARCHAR(50) NOT NULL,
 crm CHAR(8) NOT NULL,
 login VARCHAR(20) NOT NULL,
-senha CHAR(6) NOT NULL
+senha CHAR(6) NOT NULL,
+CONSTRAINT Fk_MedicoEspecialidade FOREIGN KEY(idEspecialidade)
+REFERENCES especialidade(idEspecialidade)
 );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 CREATE TABLE consulta(
 idConsulta INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -75,4 +60,22 @@ REFERENCES recepcionista(idRecepcionista),
 CONSTRAINT Fk_ConsultaMedico FOREIGN KEY(idMedico)
 REFERENCES medico(idMedico)
 );
+
+CREATE TABLE exame(
+idExame INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+idConsulta INT NOT NULL,
+dataHoraExame DATETIME NOT NULL,
+nomeExame VARCHAR(30) NOT NULL,
+statusExame VARCHAR(10) NOT NULL,
+resultado VARCHAR(200) NULL,
+dataRetiradaExane DATETIME NULL,
+dataPrevisaoEntrega DATETIME NULL,
+CONSTRAINT Fk_exameconsulta FOREIGN KEY(idConsulta)
+REFERENCES consulta(idConsulta)
+);
+
+
+
+
+
  
