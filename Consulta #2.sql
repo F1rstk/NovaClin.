c9526b0ba84e1551b6188936a48790a7c62fe49c
Filipe <filipe.sdiniz@senacsp.edu.br>
2024-06-21 15:34:49 -0300

    Dados Pacientes-Concluidos

--- Consulta #2.sql
+++ Consulta #2.sql
@@ -1,7 +1,7 @@
 
 
 SHOW TABLES
-SELECT*FROM
+SELECT*FROM paciente
 
  CREATE TABLE recepcionista(
  idRecepcionista INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -25,7 +25,7 @@ numero VARCHAR(6) NOT NULL,
 complemento VARCHAR(10) NULL,
 cidade VARCHAR(20) NOT NULL,
 cep CHAR(8) NULL,
-observações VARCHAR(100)
+observações VARCHAR(100) 
 );
 
 CREATE TABLE especialidade(
@@ -74,8 +74,23 @@ CONSTRAINT Fk_exameconsulta FOREIGN KEY(idConsulta)
 REFERENCES consulta(idConsulta)
 );
 
+ALTER TABLE paciente
+ADD estado CHAR(2) NOT NULL;
+
+INSERT INTO paciente
+VALUES
+(1,'Donald','54421170801','13993445521','2000-03-01 13:00','DonaldTrump17@gmail.com','Rua Carlos Gomes','17','Casa','Santos','11085900',NUll,'SP'),
+(2,'Margarida','32244415001','13993719011','2002-10-01 8:00','MargaridaBonitinha@gmail.com','Rua Jardim Clemente','44','Ap','São Vicente','22576700',NULL,'SP'),
+(3,'Patinhas','21135683069','13999177427','2006-02-19 14:00','DuckDickBig@gmail.com','Rua Guarani','90','Casa','Santos','11350480',NULL,'SP'),
+(4,'Hugiho','30996090053','13969899888','1999-11-20 13:30','HugoCRF128@gmail.com','Avenida Nove de Abril','10','Casa','Cubatão','11533250',NULL,'SP'),
+(5,'Luizinho','91416842063','13983467621','2001-12-23 16:00','LuizGaginho@gmail.com','Avenida Kennedy','24','Casa','Praia Grande','11010911',NULL,'SP'),
+(6,'Zezinho','89214350000','38984896854','2003-09-21','ZéZe234@gmail.com','Rua dos Girassóis','45','Casa','São João Del Rei','36305288',NULL,'MG'),
+(7,'Mickey','69834733020','38984896854','1998-04-12','DisneyDONO@gmail.com','Rua Carinhanha','55','Casa','Rio de Janeiro','21750560',NULL,'RJ'),
+(8,'Minnie','61904674070','16973297121','1999-01-20','EsposaMickey@gmail.com','Rua Alice Macuco Alves','34','Casa','São paulo','05453010',NULL,'SP');
 
 
+INSERT INTO recepcionista(nomeRecepcionista,login,senha,celular,cpf)
+('Chico
 
 
  
