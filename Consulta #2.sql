cf31086e5588de15f2269f925709038f35fba45b
Filipe <filipe.sdiniz@senacsp.edu.br>
2024-06-24 13:40:48 -0300

    ATV-Finalizada

--- Consulta #2.sql
+++ Consulta #2.sql
@@ -122,5 +122,92 @@ INSERT INTO consulta(idConsulta,idPaciente,idMedico,idRecepcionista,dataHoraCons
 VALUES(6,3,1,1,'2024-06-26 17:00','Dores no Peito',NULL),
 (7,3,2,1,'2024-06-26 11:00','Dores nos Pés',NULL);
 
+
+/*questão 7-.Inserir um exame para cada consulta do Patinhas criada no item anterior (6)*/
+INSERT INTO exame (idConsulta, dataHoraExame, nomeExame, statusExame)
+VALUES 
+(3, '2024-06-30 15:30:00', 'Eletrocardiograma', 'Pendente'),
+(5, '2024-07-01 16:30:00', 'Raio-X Coluna', 'Pendente');
+
+/*questão 8-Remarcar (Atualizar) a consulta do Donald para a mesma data porém, 2hs mais tarde.*/
+UPDATE consulta
+SET dataHoraConsulta = DATE_ADD(dataHoraConsulta, INTERVAL 2 HOUR)
+WHERE idConsulta = 1;
+
+/*questão 9-A Minie mudou de endereço mas não mudou de cidade (logradouro, numero e
+complemento). Realize a alteração dos dados.*/
+UPDATE paciente
+SET logradouro = 'nova rua', numero = '456', complemento = 'ap10'
+WHERE nome = 'Minie';
+
+/*questão 10-O Mickey mudou o celular, realize a alteração.*/
+UPDATE paciente
+SET cel = '999888777'
+WHERE nome = 'Mickey';
+
+/*questão 11-O Patinhas não poderá comparecer na consulta com o Ze Carioca. Realize a exclusão da
+mesma no sistema. Foi possível? Justifique.*/
+DELETE FROM consulta
+WHERE idPaciente = 3 AND idMedico = 3;
+/*motivo do erro*/
+/*Esta operação pode falhar devido a uma restrição de chave estrangeira. A consulta do Patinhas com Zé Carioca pode estar referenciada em outras tabelas (por exemplo, na tabela exame através da chave estrangeira idConsulta). Antes de excluir, seria necessário remover as dependências nesses registros relacionados*/
+
+/*questão 12-Realize a exclusão da Dr Mônica da tabela Medico. Foi possível? Justifique.*/
+DELETE FROM medico
+WHERE nome = 'Mônica';
+/*Não é possível excluir a médica Mônica diretamente devido a restrições de chave estrangeira. Ela pode estar associada a consultas na tabela consulta ou a exames na tabela exame. Para excluir, primeiro seria necessário remover todas as dependências relacionadas a ela nessas tabelas.*/
+
+/*questão 13-Exibir a data de todas as consultas em ordem cronológica, da que está mais próxima para a
+que está mais longe.*/
+SELECT dataHoraConsulta
+FROM consulta
+ORDER BY dataHoraConsulta;
+
+/*questão 14-Exibir todos os dados da tabela médico.*/
+SELECT * FROM medico;
+
+/*questão 15-Exibir apenas as cidades em que a clínica possui pacientes, em ordem alfabética.*/
+SELECT DISTINCT cidade
+FROM paciente
+ORDER BY cidade;
+
+/*questão 16-Exibir nome, celular e e-mail de todos os pacientes da clínica, em ordem alfabética.*/
+SELECT nome, cel, email
+FROM paciente
+ORDER BY nome;
+
+/*questão 17-Atualizar apenas o CRM do Dr.Pardal que foi cadastrado errado no sistema.*/
+UPDATE medico
+SET crm = '765544SP'
+WHERE nome = 'Pardal';
+
+/*questão 18-Resetar a senha de todos os médicos do sistema para o padrão "DOCTOR".*/
+UPDATE medico
+SET senha = 'DOCTOR';
+
+/*questão 19-Exibir apenas o nome do médico e seu CRM, dos médicos registrados em SP, tudo isto em
+ordem alfabética.*/
+SELECT nome, crm
+FROM medico
+WHERE crm LIKE '%SP%'
+ORDER BY nome;
+
+/*questão 20-Exibir nome e celular de todos os pacientes que vivem em Santos e possuem nome
+iniciando com a letra P.*/
+SELECT nome, cel
+FROM paciente
+WHERE cidade = 'Santos' AND nome LIKE 'P%'
+ORDER BY nome;
+
+
+/*desafio- Exibir o nome, logradouro, numero e cidade de todos os pacientes que moram em
+casa, em ordem alfabética.*/
+SELECT nome, logradouro, numero, cidade
+FROM paciente
+WHERE logradouro LIKE '%casa%'
+ORDER BY nome;
+
+
+
  
  
